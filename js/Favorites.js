2cf4ae256a3f386f38756f053699c7986be2ed28
Marcus Vinicius <marcusv892@gmail.com>
2023-01-31 22:02:23 -0300

    Finalizando lógica da aplicação

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,41 +1,90 @@
+export class ApiGithubUser{
+    static search(username){
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint).then(data => data.json()).then(({login, name, public_repos, followers, bio}) => ({
+            login, 
+            name,
+            public_repos,
+            followers,
+            bio
+        }))
+    }
+}
+
 export class FavoritesUsers {
     constructor(root){
        this.root = document.querySelector(root)
-       this.loading() 
+       this.loadingDataUsers()
+    }
+    
+    async addUser(username){
+        try{
+
+            if(username == ''){
+                throw new Error('Insira nome de um usuário')
+            }
+
+            const verifUserExists = this.users.find(user => user.login == username  )
+
+            if(verifUserExists){
+                throw new Error('Usuário já inserido')
+            }
+
+            const user = await ApiGithubUser.search(username)
+
+            if(user.login == undefined){
+                throw new Error('Usuário não encontrado')
+            }
+
+            this.users = [user, ...this.users]
+            this.updateView()
+            this.saveEstorage()
+        }catch(error){
+            alert(error.message)
+        }
     }
 
-    loading(){
-        this.users = [
-            {
-            login: 'viniseven',
-            name: 'Marcus Vinícius',
-            repositories: 25,
-            followers: 110,
-            email: 'marcusv892@gmail.com'  
-            },
-            {
-            login: 'diego3g',
-            name: 'Diego Fernandes',
-            repositories: 42,
-            followers: 1423,
-            email: 'diegofer3g@gmail.com'  
-            }
-        ] 
+    saveEstorage(){
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.users))
+    }
+
+    loadingDataUsers(){
+        this.users = JSON.parse(localStorage.getItem('@github-favorites:')) || []
     }
 }
 
 export class FavoritesViewUsers extends FavoritesUsers{
     constructor(root){
         super(root)
-        this.updateview()
+        this.divUser = this.root.querySelector('.class-wrapper')
+        this.updateView()
+        this.handleAddUser()
     }
 
-    updateview(){
+    handleAddUser(){
+        const btnAddUser = this.root.querySelector('.action-user button')
+        btnAddUser.onclick = () => {
+            const valueInputSearch = this.root.querySelector('.action-user input').value
+            this.addUser(valueInputSearch)
+            document.querySelector('.action-user input').value = ''
+        }
+    }
+
+    updateView(){
        this.removeAllCards()
 
         this.users.forEach( user  => {
            const data = this.createCard()
-           console.log(data)
+           
+           data.querySelector('h3').textContent = user.name
+           data.querySelector('img').src = `https://github.com/${user.login}.png`
+           data.querySelector('img').alt = `Foto de perfil do github do ${user.login}`
+           data.querySelector('#repos_user span').textContent = user.public_repos
+           data.querySelector('#followers_user span').textContent = user.followers
+           data.querySelector('#bio_user').textContent = user.bio
+
+           this.divUser.append(data)
         })
     }
 
@@ -46,18 +95,16 @@ export class FavoritesViewUsers extends FavoritesUsers{
          divCard.innerHTML = `
                 <h3>Marcus Vinicius</h3>
                 <img src="https://github.com/maykbrito.png" alt="">
-                <p>Repositories:<span>56</span></p>
-                <p>Followers:<span>5356</span></p>
-                <p>Email:<span>maykbrito.dev@gmail.com</span></p>
+                <p id="repos_user">Repositories:<span>56</span></p>
+                <p id="followers_user">Followers:<span>5356</span></p>
+                <p id="bio_user"></p>
         `
        return divCard
     }
 
     
     removeAllCards(){
-        const divUser = this.root.querySelector('.class-wrapper')
-
-        divUser.querySelectorAll('div').forEach((div) => {
+        this.divUser.querySelectorAll('div').forEach((div) => {
             div.remove()
         })
     }
